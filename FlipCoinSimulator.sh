35cff355b90ab6dcbb153b4c557d25e2d8f39929
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-08 13:13:04 +0530

    Added:To check the head or tail winner

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,1 +1,11 @@
-echo "-------------------------------------------------------------------------------WEL-COME FLIP COIN SIMULATER-------------------------------------------------------------------------------"
+#CONSTANT
+HEAD=0
+
+#TO PRINT THE HEAD AND TAIL
+flipCoin=$((RANDOM%2))
+if [ $flipCoin == $HEAD ]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
